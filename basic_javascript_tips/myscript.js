7a938ad7b592ff071ddd3586fe3032890d09bda6
Suino <violet1101@gmail.com>
2013-12-24 01:50:17 +0900

    #20 random

--- basic_javascript_tips/myscript.js
+++ basic_javascript_tips/myscript.js
@@ -1,7 +1,14 @@
-if (-1) {
-    console.log('-1 = true');
-} else {
-    console.log('-1 = false');
-}
+// 0.0 <= Math.random() < 1.0
 
-// 数値: 0 or Nan = false
+// 0-9のランダムな整数値
+var x = Math.floor(Math.random() * 10);
+
+// 1-6のランダムな整数値
+// *6 0.0 <= Math.random() < 6.0
+// floor: 与えた値以下の最大の整数
+// 0, 1, 2, 3, 4, 5
+// +1 => 1, 2, 3, 4, 5, 6
+var y = Math.floor(Math.random() * 6) + 1;
+
+console.log(x);
+console.log(y);
