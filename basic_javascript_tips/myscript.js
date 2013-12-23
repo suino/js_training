438b47ab789bf21c35041c00671798d610ec32cc
Suino <violet1101@gmail.com>
2013-12-24 02:23:35 +0900

    #24 Add DOM (2)

--- basic_javascript_tips/myscript.js
+++ basic_javascript_tips/myscript.js
@@ -10,3 +10,12 @@ fourthLink.href = 'http://google.com';
 var fourthText = document.createTextNode('Four');
 //myList.appendChild(fourthLi).appendChild(fourthText);
 myList.appendChild(fourthLi).appendChild(fourthLink).appendChild(fourthText);
+
+var fifthLi = fourthLi.cloneNode(true);
+fifthLi.innerHTML = '<a href="http://yahoo.co.jp">Five</a>';
+
+// 4の後に追加される
+// myList.appendChild(fifthLi);
+
+// 5を4の前に追加する
+myList.insertBefore(fifthLi, fourthLi);
