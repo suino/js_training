9e8df64befb073dc0ec91362e6299ce55b7c93d3
Suino <violet1101@gmail.com>
2013-12-24 02:19:38 +0900

    #23 Add DOM (1)

--- basic_javascript_tips/myscript.js
+++ basic_javascript_tips/myscript.js
@@ -3,3 +3,10 @@ var list = document.getElementsByTagName('li');
 console.log(myList);
 console.log(list);
 console.log(list[1]);
+
+var fourthLi = document.createElement('li');
+var fourthLink = document.createElement('a');
+fourthLink.href = 'http://google.com';
+var fourthText = document.createTextNode('Four');
+//myList.appendChild(fourthLi).appendChild(fourthText);
+myList.appendChild(fourthLi).appendChild(fourthLink).appendChild(fourthText);
