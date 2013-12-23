2c2ae42f59208539f85ae226c93c255d922dafdd
Suino <violet1101@gmail.com>
2013-12-24 02:31:55 +0900

    #26 change class of DOM

--- basic_javascript_tips/myscript.js
+++ basic_javascript_tips/myscript.js
@@ -1,6 +1,2 @@
 var e = document.getElementById('msg');
-e.style.color = 'red';
-e.style.fontWeight = 'bold';
-e.style.background = 'green';
-e.style.fontSize = '24px';
-e.style.border = '8px solid orange';
+e.className = 'myStyle';
