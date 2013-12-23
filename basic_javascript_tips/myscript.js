225dd8e2e3d90d67630219a890dfe9cef220d627
Suino <violet1101@gmail.com>
2013-12-24 01:58:33 +0900

    #21 event 01

--- basic_javascript_tips/myscript.js
+++ basic_javascript_tips/myscript.js
@@ -1,14 +1,4 @@
-// 0.0 <= Math.random() < 1.0
-
-// 0-9のランダムな整数値
-var x = Math.floor(Math.random() * 10);
-
-// 1-6のランダムな整数値
-// *6 0.0 <= Math.random() < 6.0
-// floor: 与えた値以下の最大の整数
-// 0, 1, 2, 3, 4, 5
-// +1 => 1, 2, 3, 4, 5, 6
-var y = Math.floor(Math.random() * 6) + 1;
-
-console.log(x);
-console.log(y);
+var e = document.getElementById('trigger');
+e.onclick = function() {
+    console.log('hi!');
+}
