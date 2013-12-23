7a9a784bc7b809b972c95896247b05a2d4cfa5fe
Suino <violet1101@gmail.com>
2013-12-24 01:26:55 +0900

    #15 difference of timer operation

--- basic_javascript_tips/myscript.js
+++ basic_javascript_tips/myscript.js
@@ -1,2 +1,20 @@
-var s = 'It\'s a small world';
-console.log(s);
+// setTimeout:  前回の処理が終わったかどうかを考慮する
+// setInterval: 考慮しない
+
+setTimeout(function () {
+    // 1秒以上の処理でクラッシュする
+}, 1000);
+
+var i = 0;
+/*
+setInterval(function () {
+    console.log(++i);
+}, 1000);
+*/
+
+// 1秒後に自分を呼び出す
+function plusOne() {
+    console.log(++i);
+    setTimeout(plusOne, 1000);
+};
+plusOne();
