7c6aae94406c6e1359dfa12d1e3a35f576af8ffc
Suino <violet1101@gmail.com>
2013-12-24 01:11:26 +0900

    #09 #10 loop break and continue

--- basic_javascript_tips/myscript.js
+++ basic_javascript_tips/myscript.js
@@ -3,10 +3,16 @@ var x = 0;
 while(x < 10) {
     console.log(x);
     x++;
+    if (x == 5) {
+        break;
+    }
 }
 
 x = 0;
 do {
-    console.log(x);
     x++;
+    if (x % 5 == 0) {
+        continue;
+    }
+    console.log(x);
 } while (x < 10);
