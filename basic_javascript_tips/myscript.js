ac43ee57e28af9d65364637d6a3ca8507214e0ac
Suino <violet1101@gmail.com>
2013-12-24 01:32:15 +0900

    #16 stop timer

--- basic_javascript_tips/myscript.js
+++ basic_javascript_tips/myscript.js
@@ -1,20 +1,19 @@
-// setTimeout:  前回の処理が終わったかどうかを考慮する
-// setInterval: 考慮しない
-
-setTimeout(function () {
-    // 1秒以上の処理でクラッシュする
-}, 1000);
-
 var i = 0;
+
 /*
-setInterval(function () {
+var timerId = setInterval(function() {
     console.log(++i);
+    if (i >= 5) {
+        clearInterval(timerId);
+    }
 }, 1000);
 */
 
-// 1秒後に自分を呼び出す
 function plusOne() {
     console.log(++i);
-    setTimeout(plusOne, 1000);
-};
+    var timerId = setTimeout(plusOne, 1000);
+    if (i >= 5) {
+        clearTimeout(timerId);
+    }
+}
 plusOne();
